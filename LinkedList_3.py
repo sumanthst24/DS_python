f3bb71e1a7e6c9a0ddb47c53c3e311362037e523
Sumanth Nidamanuri <45101690+sumanthst24@users.noreply.github.com>
2020-07-04 12:40:37 +0530

    Insert Node at end
    A program to insert a node at the end of the linked list

---
+++ LinkedList_3.py
@@ -0,0 +1,41 @@
+#Insertion of Node at beginning of the linked list
+
+class Node:
+        def __init__(self,dataVal=None):
+                self.dataval=dataVal
+                self.nextval=None
+
+        
+class LinkedList:
+        def __init__(self):
+                self.headval=None
+
+        def traverse(self):
+                printval=S.headval
+                while(printval!=None):
+                        print(printval.dataval)
+                        printval=printval.nextval
+
+
+        def insertAtEnd(self,new):
+                NewNode=Node(new)
+                currentnode=S.headval
+                while(currentnode.nextval!=None):
+                        currentnode=currentnode.nextval
+                currentnode.nextval=NewNode
+                        
+
+
+S=LinkedList()
+S.headval=Node(1)
+q=Node(2)
+r=Node(3)
+S.headval.nextval=q
+q.nextval=r
+t=Node(4)
+r.nextval=t
+print("before insertion")
+S.traverse()
+S.insertAtEnd(5)
+print("After insertion at end")
+S.traverse()
