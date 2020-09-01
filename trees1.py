7f0c8ad4497cbba8afe5d975d869bd9ddf1e0da4
Sumanth Nidamanuri <45101690+sumanthst24@users.noreply.github.com>
2020-09-01 16:32:43 +0530

    create a root node and display it

---
+++ trees1.py
@@ -0,0 +1,17 @@
+class Node:
+
+    def __init__(self,data):
+
+        self.data=data
+        self.left=None
+        self.right=None
+
+        
+    def Print(self):
+
+        print(self.data)
+
+
+p=Node(5)
+p.Print()
+        
